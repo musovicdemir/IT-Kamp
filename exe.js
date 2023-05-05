c944e15b1f4155759d0980526468bd78915796a5
musovicdemir <demcolone322@gmail.com>
2023-05-05 10:56:50 +0200

    1

---
+++ exe.js
@@ -0,0 +1,161 @@
+// let a, b, c;
+// a = 12;
+// b = 13;
+// c = a + b;
+
+// console.log(c);
+
+// if (c < 24) {
+//     console.log(c)
+// } else {
+//     console.log('Error')
+// }
+
+// function describeAge(age) {
+//     if (age <= 12) {
+//       return "You're a(n) kid";
+//     } else if (age >= 13 && age <= 17) {
+//       return "You're a(n) teenager";
+//     } else if (age >= 18 && age <= 64) {
+//       return "You're a(n) adult";
+//     } else {
+//       return "You're a(n) elderly";
+//     }
+//   }
+// console.log(describeAge(20));
+
+// function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return results;
+//   }
+// console.log(mystery())
+
+//     function makeNegative(n) {
+//         return n - n * 2;
+//     }
+
+// console.log(makeNegative(-5))
+
+// function howManyLightsabersDoYouOwn(name) {
+//     if name === "Zach" {
+//         return 18;
+//     }
+// }
+
+// console.log(howManyLightsabersDoYouOwn(Zach))
+
+// function greet(a) {
+//     return "hello world";
+// }
+
+// console.log(greet());
+// function hello(name) {
+//     if (name === undefined) {
+//         return 'Hello, '+'World'+'!';
+//     } 
+//     if (name === null) {
+//         return 'Hello, '+'World'+'!';
+//     }
+//     if (name === '') {
+//         return 'Hello, '+'World'+'!';
+//     }
+//     else {
+//         return 'Hello, '+name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()+'!';
+// }
+//   }
+
+//   console.log(hello('dEmirR'))
+
+// function square(a) {
+//     return a*a;
+// }
+// console.log(square(4))
+
+// function evenOrOdd(number) {
+//     if (number % 2 == 0) {
+//         return 'Even';
+//     }
+//     else {
+//         return 'Odd';
+//     }
+// }
+
+// console.log(evenOrOdd(2))
+
+// var greet_abe = function() {
+//     var name = 'Abe';
+//   return "Hello, " + name + '!';
+// };
+// var greet_ben = function() {
+//     var name = 'Ben';
+//   return "Hello, " + name + '!';
+// };
+// console.log(greet_ben())
+
+// function solution(str){
+//   var split = str.split("");
+//   var reverse = split.reverse();
+//   var join = reverse.join("");
+//   return join;
+// }
+
+// console.log(solution('hello'))
+
+// function true1() {
+//   console.log("True");
+// }
+
+// function false1() {
+//   console.log("False");
+// }
+
+
+// console.log(if1(true))
+
+// function simpleMultiplication(number) {
+//   if (number % 2 == 0) {
+//     return number * 8;
+//   } else {
+//     return number * 9;
+//   }
+// }
+
+// console.log(simpleMultiplication(3))
+
+// function periodIsLate(last, today, cycleLength) {
+//     if (last - today > cycleLength) {
+//         return "True";
+//     } else {
+//         return "false";
+//     }
+//   }
+
+//   console.log(periodIsLate(15,20,9))
+
+// function noSpace(x){
+//     split = x.split(" ").join("");
+//     return split;
+// }
+
+// console.log(noSpace("wea wewa  wwewe  we"))
+
+// function removeNoise(str){
+//     strR = str.split("").filter("%$&/#·@|º\ª","");
+//     return strR;
+// }
+
+// console.log(removeNoise("h%e&·%$·llo w&%or&$l·$%d"
+
+// fizz buzz
+
+// for (let  = 0;  < i < 100; i++) {
+//     if (i / 3) {
+//         console.log("FIZZ")
+//     } else if (i / 5) {
+//         console.log("BUZZ")
+//     } else (i / 3, i / 5); {
+//         console.log("BUZZFIZZ")
+//     }
+// }
+
---
+++ js.js
@@ -0,0 +1,72 @@
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + 15)
+//     } else if (i % 3 == 0) {
+//         console.log(i + 10)
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+// function sqrNum(a) {
+//     return a*a;
+// }
+
+// console.log(sqrNum(5));
+
+// function func1(a,b) {
+//     if (a < b) {
+//         return b - a;
+//     } else {
+//         return a - b;
+//     }
+// }
+
+// console.log(func1(5,10));
+
+// function ctoF(a) {
+//     return a - 32 / 1.8;
+// }
+
+// console.log(ctoF(50));
+
+// let newArr = ["BMW","AUDI","MERCEDES","FIAT"]
+
+// newArr.push("ALFA","VW")
+
+// newArr.unshift("PORSCHE","BUGHATTI")
+
+// console.log(newArr);
+
+// let numbers = [1, 3, 5, 7, 9];
+
+// for (let i = 0; i < 10; i++) {
+//     if ( i % 2 === 0 && i !== 0)
+//     numbers.push(i);
+// }
+
+// console.log(numbers)
+// console.log(numbers.reverse())
+
+// let myLastName
+
+// for( let i = 0 ; i < 5 ; i++){
+//     if (i === 3) {
+//         myLastName = "aldin"
+//     }
+//     if (myLastName) {
+//         console.log("imam ime", i)
+//     } else {
+//         console.log("sacekaj redzi", i)
+//     }
+// }
+// let b = 3;
+// {
+//     var a;
+//     let b = 10;
+//     const c = 15;
+//     {
+//         console.log(b,'blok koda')
+//         console.log(c,'blok koda')
+//     }
+// }
