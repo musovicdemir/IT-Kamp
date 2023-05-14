b1c2954e6de17f7349d3a2189eda9bc2dafa7255
musovicdemir <demcolone322@gmail.com>
2023-05-14 17:52:52 +0200

    vezba

--- js.js
+++ js.js
@@ -12,6 +12,126 @@
 //     return a*a;
 // }
 
+function describeAge(age) {
+    if (age <= 12) {
+      return "You're a(n) kid";
+    } else if (age >= 13 && age <= 17) {
+      return "You're a(n) teenager";
+    } else if (age >= 18 && age <= 64) {
+      return "You're a(n) adult";
+    } else {
+      return "You're a(n) elderly";
+    }
+  }
+console.log(describeAge(20));
+
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
+console.log(mystery())
+
+    function makeNegative(n) {
+        return n - n * 2;
+    }
+
+console.log(makeNegative(-5))
+
+function howManyLightsabersDoYouOwn(name) {
+    if name === "Zach" {
+        return 18;
+    }
+}
+
+console.log(howManyLightsabersDoYouOwn(Zach))
+
+function greet(a) {
+    return "hello world";
+}
+
+console.log(greet());
+function hello(name) {
+    if (name === undefined) {
+        return 'Hello, '+'World'+'!';
+    } 
+    if (name === null) {
+        return 'Hello, '+'World'+'!';
+    }
+    if (name === '') {
+        return 'Hello, '+'World'+'!';
+    }
+    else {
+        return 'Hello, '+name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()+'!';
+}
+  }
+
+  console.log(hello('dEmirR'))
+
+function square(a) {
+    return a*a;
+}
+console.log(square(4))
+
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
+        return 'Even';
+    }
+    else {
+        return 'Odd';
+    }
+}
+
+console.log(evenOrOdd(2))
+
+var greet_abe = function() {
+    var name = 'Abe';
+  return "Hello, " + name + '!';
+};
+var greet_ben = function() {
+    var name = 'Ben';
+  return "Hello, " + name + '!';
+};
+console.log(greet_ben())
+
+function solution(str){
+  var split = str.split("");
+  var reverse = split.reverse();
+  var join = reverse.join("");
+  return join;
+}
+
+console.log(solution('hello'))
+
+function true1() {
+  console.log("True");
+}
+
+function false1() {
+  console.log("False");
+}
+
+
+console.log(if1(true))
+
+function simpleMultiplication(number) {
+  if (number % 2 == 0) {
+    return number * 8;
+  } else {
+    return number * 9;
+  }
+}
+
+console.log(simpleMultiplication(3))
+
+function periodIsLate(last, today, cycleLength) {
+    if (last - today > cycleLength) {
+        return "True";
+    } else {
+        return "false";
+    }
+  }
+
 // console.log(sqrNum(5));
 
 // function func1(a,b) {
