e13360822f81633de866c66ce5800b54ee7204d6
musovicdemir <demcolone322@gmail.com>
2023-05-19 18:21:41 +0200

    vezba

--- js.js
+++ js.js
@@ -307,3 +307,22 @@
 
 // callFunction.call(myCar,"je", "godiste");
 
+let a = 10;
+
+function firstA() {
+  let b = 5;
+  console.log(b);
+}
+
+function secondA() {
+  let b = 3;
+  console.log(b);
+}
+
+function lastA() {
+  console.log(a);
+}
+
+firstA();
+secondA();
+lastA();
